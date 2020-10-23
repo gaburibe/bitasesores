2c38b5b72ecc8eea17b1519dafe77a25b6f18312
gaburibe <gaburibe@gmail.com>
2020-10-23 12:22:02 -0500

    next

--- index.js
+++ index.js
@@ -32,8 +32,9 @@ app.use('/bandeja/', express.static(__dirname + '/bandeja'));
 app.use('/ordenes/', express.static(__dirname + '/ordenes'));
 app.use('/archivo/', express.static(__dirname + '/archivo'));
 app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "http://bitasesores.tech/"); // update to match the domain you will make the request from
+res.header("Access-Control-Allow-Origin", "http://bitasesores.tech"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
@@ -87,7 +88,7 @@ app.post("/menu", function(req,res){
 
 
 app.get('/', function (req, res) {
-	
+		console.log("miau");
 	   res.sendFile(path.join(__dirname + '/sitio/cuestionario.html'));
 
 
