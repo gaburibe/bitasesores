cd5e22f7bf2d3f61d8d009fe87af404c4fe90dc5
gaburibe <gaburibe@gmail.com>
2020-10-23 13:27:03 -0500

    s

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use('/ordenes/', express.static(__dirname + '/ordenes'));
 app.use('/archivo/', express.static(__dirname + '/archivo'));
 
 const corsOptions = {
-  origin: ['http://localhost:4949','http://bitasesores.tech','http://docfresco.com']
+  origin: 'http://bitasesores.tech'
 }
 
 app.use(cors(corsOptions))
