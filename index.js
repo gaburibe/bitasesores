52e9ae9c5dae0d9dd15f47bd48a1a7d93cd28123
gaburibe <gaburibe@gmail.com>
2020-10-23 13:06:07 -0500

    d

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.use('/', express.static(__dirname + '/sitio'));
 app.use('/bandeja/', express.static(__dirname + '/bandeja'));
 app.use('/ordenes/', express.static(__dirname + '/ordenes'));
 app.use('/archivo/', express.static(__dirname + '/archivo'));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://docfresco.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Origin", "http://bitasesores.tech"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
